78a45c88f1b71a1f183749cb93d7f96a3aad6eee
ArthurLeonard <ialeonard@ucdavis.edu>
2019-02-25 14:58:23 -0800

    mvp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import './App.css';
-import { Home, About, Contact, Navigation } from './components';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "./App.css";
+import { Home, About, Contact, Navigation } from "./components";
 
 const App = () => (
   <div>
     <Navigation />
+
+    <Route exact path="/" component={Home} />
+    <Route path="/Contact" component={Contact} />
+    <Route path="/About" component={About} />
   </div>
 );
 
